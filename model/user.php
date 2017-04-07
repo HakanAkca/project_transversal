1ddd7161abfb587e45832dc34cf4e80abee1abb3
cheikhomarsow <cheikhomar60@gmail.com>
2017-04-07 12:53:08 +0200

    register regexp

--- model/user.php
+++ model/user.php
@@ -11,35 +11,52 @@ function get_user_by_id($id)
 
 function get_user_by_username($username)
 {
-    $data = find_one_secure("SELECT * FROM users WHERE username = :username",
+    $data = find_one_secure("SELECT * FROM users WHERE Pseudo = :username",
                             ['username' => $username]);
     return $data;
 }
 
 function user_check_register($data)
 {
-    if (empty($data['username']) OR empty($data['password'])){
+    if (empty($data['username']) OR empty($data['password'])
+        OR empty($data['verifpassword']) OR empty($data['email'])
+        OR empty($data['city'])){
         return false;
     }
-        
-    /*$data2 = get_user_by_username($data['username']);
+    if(strlen($data['username'])<6){
+        return false;
+    }
+
+    $data2 = get_user_by_username($data['username']);
     if ($data2 !== false){
         return false;
     }
-    //TODO : Check valid username, password, secret ask
-    /*if(strlen($data['username'])<6){
+    if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
         return false;
     }
+
+    if(!validatePASSWORD($data['password'])){
+        return false;
+    }
+
     if($data['password'] !== $data['verifpassword']){
         return false;
     }
-    */
-    /*$regexp = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/";
-    if(!preg_match($regexp, $data['password']))
-        return false;*/
+
     return true;
 }
 
+function validatePASSWORD($PASSWORD){
+    $uppercase = preg_match('@[A-Z]@', $PASSWORD);
+    $lowercase = preg_match('@[a-z]@', $PASSWORD);
+    $number    = preg_match('@[0-9]@', $PASSWORD);
+
+    if(!$uppercase || !$lowercase || !$number || strlen($PASSWORD) < 8) {
+        return false;
+    }else{
+        return true;
+    }
+}
 function user_hash($pass)
 {
     $hash = password_hash($pass, PASSWORD_BCRYPT, ['salt' => 'saltysaltysaltysalty!!']);

99b59b2b8002b07020756f04ffd3ab68df482f7e
Nathael Arki <nathael.arki@supinternet.fr>
2017-04-12 12:49:21 +0200

    Maj

--- assets/register.js
+++ assets/register.js
@@ -1,13 +1,5 @@
-window.onload = function(){
-    registerForm = document.querySelector('#myRegister');
-    registerForm.onsubmit = function(){
-        var username = registerForm.querySelector('input[name=username]').value;
-        var email = registerForm.querySelector('input[name=email]').value;
-        var password = registerForm.querySelector('input[name=password]').value;
-        var verifpassword = registerForm.querySelector('input[name=verifpassword]').value;
-        var city = registerForm.querySelector('input[name=city]').value;
-        
-        console.log(username, email, password, verifpassword, city);
-        return false;
-    }
-}
+$(function() {
+    jQuery('.form-container-12').foxholder({
+        demo: 2 //or other number of demo (1-15) you want to use
+    });
+});
--- views/register.php
+++ views/register.php
@@ -3,7 +3,10 @@
     <head>
         <title>Register</title>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="assets/foxholder-styles.css" />
         <link href="web/register.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/foxholder.js"></script>
         <script src="assets/register.js"></script>
     </head>
     <body>
@@ -25,13 +28,23 @@
             INSCRIPTION
         </div>
         <div id="line"></div>
-        <form id="myRegister" method="POST" action="?action=register" class="form">
-            <input type="text" name="username" placeholder="Pseudo*"><br>
-            <input type="email" name="email" placeholder="Mail*"><br>
-            <input type="password" name="password" placeholder="Mot de Passe*"><br>
-            <input type="password" name="verifpassword" placeholder="Confirmation Mot de Passe*"><br>
-            <input type="text" name="city" placeholder="Ville*"><br>
-            <input type="submit" value="Créer mon compte" id="button" >
-        </form>
+        <!--<form id="myRegister" class="form" method="POST" action="?action=register">
+                <input type="text" name="username" id="first-input-1" placeholder="Pseudo*"><br>
+                <input type="email" name="email" placeholder="Mail*"><br>
+                <input type="password" name="password" placeholder="Mot de Passe*"><br>
+                <input type="password" name="verifpassword" placeholder="Confirmation Mot de Passe*"><br>
+                <input type="text" name="city" placeholder="Ville*"><br>
+                <button type="submit" id="button">Créer mon compte</button>
+            </form>-->
+        <div class="form-container-12 form-container form" id="example-12">
+            <form id="myRegister" method="POST" action="?action=register">
+                <input id="username" type="text" name="username" placeholder="Pseudo*">
+                <input id="email" type="email" name="email" placeholder="Mail*">
+                <input id="password" type="password" name="password" placeholder="Mot de Passe*">
+                <input id="verifpassword" type="password" name="verifpassword" placeholder="Confirmation Mot de Passe*">
+                <input id="city" type="text" name="city" placeholder="Ville*">
+                <button type="submit" id="button">Créer mon compte</button>
+            </form>
+        </div>
     </body>
 </html>

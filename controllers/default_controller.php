8b276131bdf33ab79c268ca2262cf53abccb93a8
SOW Cheikh Omar <cheikh-omar.sow@supinternet.fr>
2017-04-12 12:43:04 +0200

    form

--- controllers/default_controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('model/user.php');
-
-function home_action()
-{
-    require('views/home.php');
-}
--- views/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<form method='POST' action='?action=home'> 
-        <label for='username'>Nom d'utilisateur : </label>
-        <input type='text' name='username' id='username'><br>
-        <label for='password'>Mot de passe : </label>
-        <input type='password' name='password' id='password'><br>
-        <input type='submit' value='Connexion'> 
-        </form>";

4989680c3f90468e0cacdb7a8e71c5d257289cdc
Nathael Arki <nathael.arki@supinternet.fr>
2017-05-10 00:40:59 +0200

    modale

--- assets/script/register.js
+++ assets/script/register.js
@@ -5,13 +5,15 @@ $(function() {
 
     $('.burger').click(function(){
         if($('.mobile_dropdown').is(':visible')){
-            $('.mobile_dropdown').animate({'left' :"-100%"}, 1000, function(){
+            $('.mobile_dropdown').animate({'left' :"-100%"}, 500, function(){
                 $('.mobile_dropdown').css({'display': 'none', 'left':'-100%'});
+                $('body').css('overflow', 'initial');
             })
         }
         else{
             $('.mobile_dropdown').css({'display': 'flex', 'left':'-100%'});
-            $('.mobile_dropdown').animate({'left' :"0"}, 1000 )
+            $('.mobile_dropdown').animate({'left' :"0"}, 500 )
+            $('body').css('overflow', 'hidden');
         }
     })
 
@@ -33,5 +35,24 @@ $(function() {
     $('#profile_sondage').click(function(){
         switch_tab($('#profile_sondage'), $('.sondage'))
     });
+
+    $('.deal_cost').click(function(){
+        $('#modale').css('display', 'flex');
+        $('body').css('overflow', 'hidden');
+       
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.img_modale').attr("src", $(this).parent().children(".partner_img").attr("src"));
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.partner_modale').html($(this).parent().children(".deal_info").children('span:first').html());
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.city_modale').html($(this).parent().children(".other_info").children('span:first').html());
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.expiration_modale').html('slt');
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.cost').children('.price').html($(this).parent().children(".deal_cost").children('span:first').html());
+        $('#modale').children('.modale_available_deal').children('.deal_info').children('.cost').children('.reduc').html($(this).parent().children(".deal_info").children('span:last').html());
+
+        $('#modale').children('.modale_available_deal').children('.action').children('.buy_deal').children('input:first').attr('value', $(this).parent().children(".other_info").children('.id').html());
+    })
+
+    $('.close_modale').click(function(){
+        $('#modale').css('display', 'none');
+        $('body').css('overflow', 'initial');        
+    })
 
 });
